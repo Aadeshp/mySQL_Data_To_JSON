d571fa7700a47a5b8122cee33f75aa38a1dd695d
Aadesh Patel <aadeshpatel@Aadeshs-MacBook-Pro.local>
2015-02-04 17:11:53 -0500

    updated example.c

--- example.c
+++ example.c
@@ -1,12 +1,12 @@
 #include "mysql_query.h"
 
 int main() {
-    MYSQL *sql = init_mysql("localhost", "root", "@patelindustries990", "codearchive_db");
-    JSON *json = query(sql, "SELECT * FROM CodeArchiveSite_syntaxminipost");
+    MYSQL *sql = init_mysql(/* DATABASE INFO HERE */);
+    JSON *json = query(sql, /*mySQL Raw Query Here*/);
     
     int i;
     for (i = 0; i < json->size; i++)
-        printf("%s\n", ht_get(json->arr_ht[i], "name"));
+        printf("%s\n", ht_get(json->arr_ht[i], /*Column Name Here*/));
 
     return 0;
 }
